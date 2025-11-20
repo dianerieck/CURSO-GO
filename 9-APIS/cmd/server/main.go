94ec724db476f5df88ded430f80c9d79dc4b0058
Diane <dianerieck@hotmail.com>
2025-11-20 14:44:27 -0300

    first commit

---
+++ 9-APIS/cmd/server/main.go
@@ -0,0 +1,86 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/dianerieck/CURSO-GO/9-APIS/configs"
+	_ "github.com/dianerieck/CURSO-GO/9-APIS/docs"
+	"github.com/dianerieck/CURSO-GO/9-APIS/internal/entity"
+	"github.com/dianerieck/CURSO-GO/9-APIS/internal/infra/database"
+	"github.com/dianerieck/CURSO-GO/9-APIS/internal/infra/webserver/handlers"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/jwtauth"
+	httpSwagger "github.com/swaggo/http-swagger"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+// @title           Go Expert Example API
+// @version         1.0
+// @description     Product API with auhtentication.
+// @termsOfService  http://swagger.io/terms/
+
+// @contact.name   Diane Elo Rieck
+// @contact.url    http://www.dianerieck.com.br
+// @contact.email  dianerieck@hotmail.com
+
+// @license.name  Diane Rieck Licence
+// @license.url   http://www.dianerieck.com.br
+
+// @host      localhost:8080
+// @BasePath  /
+
+// @securityDefinitions.apikey ApiKeyAuth
+// @in header
+// @name Authorization
+
+func main() {
+	configs, err := configs.LoadConfig(".")
+	if err != nil {
+		panic(err)
+	}
+	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+	db.AutoMigrate(&entity.Product{}, &entity.User{})
+	productDB := database.NewProduct(db)
+	productHandler := handlers.NewProductHandler(productDB)
+
+	userDB := database.NewUser(db)
+	userHandler := handlers.NewUserHandler(userDB)
+
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+	r.Use(middleware.WithValue("jwt", configs.TokenAuth))
+	r.Use(middleware.WithValue("JWTExperesIn", configs.JWTExperesIn))
+	//r.Use(LogRequest)
+
+	r.Route("/products", func(r chi.Router) {
+		r.Use(jwtauth.Verifier(configs.TokenAuth))
+		r.Use(jwtauth.Authenticator)
+		r.Post("/", productHandler.CreateProduct)
+		r.Get("/", productHandler.GetProducts)
+		r.Get("/{id}", productHandler.GetProduct)
+		r.Put("/{id}", productHandler.UpdateProduct)
+		r.Delete("/{id}", productHandler.DeleteProduct)
+
+	})
+	r.Post("/users", userHandler.Create)
+	r.Post("/users/generate_token", userHandler.GetJWT)
+	r.Get("/docs/*", httpSwagger.Handler(httpSwagger.URL("http://localhost:8080/docs/doc.json")))
+	http.ListenAndServe(":8080", r)
+
+}
+
+//func LogRequest(next http.Handler) http.Handler {
+//	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+//		r.Context().Value("user")
+//		log.Println(r.Method, r.URL.Path)
+//		next.ServeHTTP(w, r)
+//	})
+//}
+
+//usar swag init no terminal para iniciar o swager e criar a pasta docs
---
+++ 9-APIS/internal/infra/webserver/handlers/user_handlers.go
@@ -0,0 +1,92 @@
+package handlers
+
+import (
+	"encoding/json"
+	"net/http"
+	"time"
+
+	"github.com/dianerieck/CURSO-GO/9-APIS/internal/dto"
+	"github.com/dianerieck/CURSO-GO/9-APIS/internal/entity"
+	"github.com/dianerieck/CURSO-GO/9-APIS/internal/infra/database"
+	"github.com/go-chi/jwtauth"
+)
+
+type Error struct {
+	Message string `json:"message"`
+}
+type UserHandler struct {
+	UserDB database.UserInterface
+}
+
+func NewUserHandler(userDB database.UserInterface) *UserHandler {
+	return &UserHandler{
+		UserDB: userDB,
+	}
+}
+
+func (h *UserHandler) GetJWT(w http.ResponseWriter, r *http.Request) {
+	//var user dto.CreateUserInput
+	jwt := r.Context().Value("jwt").(*jwtauth.JWTAuth)
+	jwtExpiresIn := r.Context().Value("JWTExperesIn").(int)
+	var user dto.GetJWTInput
+	err := json.NewDecoder(r.Body).Decode(&user)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	u, err := h.UserDB.FindByEmail(user.Email)
+	if err != nil {
+		w.WriteHeader(http.StatusUnauthorized)
+		return
+	}
+	if !u.ValidatePassword(user.Password) {
+		w.WriteHeader(http.StatusUnauthorized)
+		return
+	}
+
+	_, tokenString, _ := jwt.Encode(map[string]interface{}{
+		"sub": u.ID.String(),
+		"exp": time.Now().Add(time.Second * time.Duration(jwtExpiresIn)).Unix(),
+	})
+
+	accessToken := struct {
+		AccessToken string `json:"acess_token`
+	}{
+		AccessToken: tokenString,
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(accessToken)
+}
+
+// Create user godoc
+// @Summary 	Create user
+// @Description Create user
+// @Tags 		user
+// @Accept 		json
+// @Produce		json
+// @Param		request  body dto.CreateUserInput true "user request"
+// @Success		201
+// @Failure 	500		{object} error
+// @Router		/users [post]
+
+func (h *UserHandler) Create(w http.ResponseWriter, r *http.Request) {
+	var user dto.CreateUserInput
+	err := json.NewDecoder(r.Body).Decode(&user)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	u, err := entity.NewUser(user.Name, user.Email, user.Password)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	err = h.UserDB.Create(u)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	w.WriteHeader(http.StatusCreated)
+}
